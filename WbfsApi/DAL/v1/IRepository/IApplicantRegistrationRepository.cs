019150634193d18bdcb40f40b45b1c6d00fe3ab2
tanmaybug <tanmay.bug@gmail.com>
2024-07-23 22:07:30 +0530

    applicant reg

---
+++ WbfsApi/DAL/v1/IRepository/IApplicantRegistrationRepository.cs
@@ -0,0 +1,15 @@
+﻿using WbfsApi.DAL.Entities;
+
+namespace WbfsApi.DAL.v1.IRepository
+{
+    public interface IApplicantRegistrationRepository
+    {
+        Task<List<WfsQualifyingExamMaster>?> GetQualifyingExams();
+
+        /*Task<List<WfsTwelfthStdBoardMaster>> GetWfsTwelfthStdBoards();
+
+        Task<List<WfsCourseMaster>> GetCourseMasters();
+
+        Task<List<WfsDistrictMaster>> GetDistrictMasters();*/
+    }
+}
---
+++ WbfsApi/DAL/v1/Repository/ApplicantRegistrationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using WbfsApi.DAL.DBContext;
+using WbfsApi.DAL.Entities;
+using WbfsApi.DAL.v1.IRepository;
+
+namespace WbfsApi.DAL.v1.Repository
+{
+    public class ApplicantRegistrationRepository : IApplicantRegistrationRepository
+    {
+        private readonly WbfsDBContext _dbContext;
+        public ApplicantRegistrationRepository(WbfsDBContext dBContext) 
+        {
+            _dbContext = dBContext;
+        }
+        public async Task<List<WfsQualifyingExamMaster>?> GetQualifyingExams()
+        {
+            var examData = await _dbContext.WfsQualifyingExamMasters.Where(p => p.ActiveStatus==1).ToListAsync();
+
+            if (examData == null)
+            {
+                return null;
+            }
+            return examData;
+        }
+    }
+}
